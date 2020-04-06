4afba9da786e4f3a743ef6a3c7326b685ed74309
Anupam <46647457+anupam8757@users.noreply.github.com>
2020-04-06 08:10:07 +0530

    find last element

---
+++ recursion_challenges/findlastelement.cpp
@@ -0,0 +1,48 @@
+/*
+Take as input N, the size of array. Take N more inputs and store that in an array. Take as input M, a number. Write a recursive function which returns the last index at which M is found in the array and -1 if M is not found anywhere. Print the value returned.
+
+Input Format
+There will be three lines of input:
+
+N - the size of the array
+N space separated integers that make up the array
+M
+Constraints
+1 < N < 1000
+-10^9 < i,M < 10^9 , where i is any number within the array
+
+Output Format
+For each case, print the integer value of the last index that M is found at within the given array.
+If it is not found, print '-1' (without the quotes).
+
+Sample Input
+7
+86 -16 77 65 45 77 28
+77
+Sample Output
+5
+Explanation
+Last occurence of 77 is found at index*/
+
+#include<iostream>
+using namespace std;
+
+int last_occur(int *a,int i,int n,int key)
+{
+    static int index=-1;
+	if(i==n)
+	return index;
+	if(a[i]==key)
+	index=i;
+	return(last_occur(a,i+1,n,key));
+}
+int main() {
+	int n,m;
+	int arr[100000];
+    cin>>n;
+	for(int i=0;i<n;i++)
+	cin>>arr[i];
+	cin>>m;
+	cout<<last_occur(arr,0,n,m);
+	return 0;
+}
